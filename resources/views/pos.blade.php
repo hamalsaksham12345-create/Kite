7a82e4a938edaae3771c946a29b951028a98fef3
Saksham Thakuri <sakshamthakuri@Sakshams-MacBook-Air.local>
2026-05-18 11:36:35 +0545

    resources: Replace dollar currency with Nepali Rs in root views

--- resources/views/pos.blade.php
+++ resources/views/pos.blade.php
@@ -115,7 +115,7 @@
                         </svg>
                     </div>
                 </div>
-                <p class="text-3xl font-black text-emerald-700">$0.00</p>
+                <p class="text-3xl font-black text-emerald-700">Rs 0.00</p>
                 <p class="text-sm font-bold text-gray-600 mt-1">Revenue</p>
             </div>
 
